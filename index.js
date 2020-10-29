41943450a5000811f05ef33ff8e57cb786e1bbfc
alinebeatrizw <alinewille31@gmail.com>
2020-10-28 22:56:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require ("express");
+const app = express();
+const handlebars = require ("express-handlebars");
+const Sequelize = require("sequelize");
+
+
+
+//config handlebars
+    //template engine
+    app.engine("handlebars", handlebars({defaultLayout:"main"}));
+    app.set("view engine", "handlebars");
+
+//conectando ao banco
+const sequelize = new Sequelize("cadastro", "root", "12345", {
+    host: "localhost",
+    dialect: "mysql"
+});
+
+
+//rotas
+
+app.get("/cad", function(req,res){
+    res.render("formulario")
+})
+
+//ativando servidor
+app.listen(8081, function(){
+    console.log("servidor rodando")
+});
